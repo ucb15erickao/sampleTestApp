65338086f02f4b18e0d59a7a09d646266c17d2bb
Eric Kao <ucberickao@gmail.com>
2023-01-25 16:52:08 +0800

    Change file naming for node server

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import datetime
-date = str(datetime.datetime.now())[:19]
+date = str(datetime.datetime.now())[:10] + '_' + str(datetime.datetime.now())[11:19]
 
 results = 'These are the results for the test run on ' + date + '.'
 print('test.py has been executed')
 
-with open(date + '_log.txt', 'w') as logFile:
+with open(date + '.log', 'w') as logFile:
   logFile.write(results)
