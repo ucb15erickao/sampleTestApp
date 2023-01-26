1e4e25a561759c2436eef84ccba6493ebd2b6b1d
Eric Kao <ucberickao@gmail.com>
2023-01-26 17:50:40 +0800

    Remove print

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ import datetime
 date = str(datetime.datetime.now())[:10] + '_' + str(datetime.datetime.now())[11:19]
 
 results = 'These are the results for the test run on ' + date + '.'
-print('test.py has been executed')
 
 with open(date + '.log', 'w') as logFile:
   logFile.write(results)
